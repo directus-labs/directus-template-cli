d889124603361d4117a48e91c0e6d6acb9b23c3e
bryantgillespie <hey@bryantgillespie.com>
2023-06-05 07:57:29 -0400

    add rate limiter

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,16 @@
 import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios'
+import Bottleneck from 'bottleneck'
 
 class Api {
   private instance: AxiosInstance;
+  private limiter: Bottleneck;
 
   constructor() {
     this.instance = axios.create()
+    this.limiter = new Bottleneck({
+      minTime: 100, // Set min time between tasks here (1000 ms = 1 second)
+      // You can set other options here as well.
+    })
   }
 
   public setBaseUrl(url: string): void {
@@ -16,23 +22,23 @@ class Api {
   }
 
   public get<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return this.instance.get<T>(url, config)
+    return this.limiter.schedule(() => this.instance.get<T>(url, config))
   }
 
   public post<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return this.instance.post<T>(url, data, config)
+    return this.limiter.schedule(() => this.instance.post<T>(url, data, config))
   }
 
   public put<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return this.instance.put<T>(url, data, config)
+    return this.limiter.schedule(() => this.instance.put<T>(url, data, config))
   }
 
   public patch<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return this.instance.patch<T>(url, data, config)
+    return this.limiter.schedule(() => this.instance.patch<T>(url, data, config))
   }
 
   public delete(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse> {
-    return this.instance.delete(url, config)
+    return this.limiter.schedule(() => this.instance.delete(url, config))
   }
 }
 
