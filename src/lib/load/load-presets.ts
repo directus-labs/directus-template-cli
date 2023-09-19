391b57776fe91848da0a3d3704b6bbabacc6ebb2
Alex van der Valk <21194068+alexvdvalk@users.noreply.github.com>
2023-09-19 17:16:31 +0100

    delete presets before loading new ones

--- src/lib/load/load-presets.ts
+++ src/lib/load/load-presets.ts
@@ -1,14 +1,26 @@
 import { api } from "../api";
 
 export default async (presets: any[]) => {
+  await deleteAllPresets();
   const cleanPresets = presets.map((preset) => {
     preset.user = null;
-    delete preset.id;
     return preset;
   });
   try {
     await api.post("presets", cleanPresets);
   } catch (error) {
     console.log("Error uploading preset", error.response.data.errors);
+  }
+};
+
+const deleteAllPresets = async () => {
+  try {
+    const { data: presets } = await api.get<any>("presets");
+    const ids = presets.data.map((preset) => preset.id);
+    await api.delete("presets", {
+      data: ids,
+    });
+  } catch (error) {
+    console.log("Error removing existing presets", error.response.data.errors);
   }
 };
