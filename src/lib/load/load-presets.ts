ddaf765531335ba4e809b069538e296efde19da0
Alex van der Valk <21194068+alexvdvalk@users.noreply.github.com>
2023-09-19 16:43:21 +0100

    remove preset ID. Load all presets in a single req

--- src/lib/load/load-presets.ts
+++ src/lib/load/load-presets.ts
@@ -1,15 +1,14 @@
-import {api} from '../api'
+import { api } from "../api";
 
 export default async (presets: any[]) => {
-  const cleanPresets = presets.map(preset => {
-    preset.user = null
-    return preset
-  })
-  for (const preset of cleanPresets) {
-    try {
-      await api.post('presets', preset)
-    } catch (error) {
-      console.log('Error uploading preset', error.response.data.errors)
-    }
+  const cleanPresets = presets.map((preset) => {
+    preset.user = null;
+    delete preset.id;
+    return preset;
+  });
+  try {
+    await api.post("presets", cleanPresets);
+  } catch (error) {
+    console.log("Error uploading preset", error.response.data.errors);
   }
-}
+};
