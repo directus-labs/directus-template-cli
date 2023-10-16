43b70256a60773d00f17f7326187b69e557f7288
bryantgillespie <hey@bryantgillespie.com>
2023-10-16 14:01:51 -0400

    fix role loading

--- src/lib/load/load-roles.ts
+++ src/lib/load/load-roles.ts
@@ -1,21 +1,26 @@
-import {api} from '../api'
+import { api } from "../api";
 
 export default async function loadRoles(roles: any) {
-  const cleanedUpRoles = roles.map(role => {
-    delete role.users
-    return role
-  })
+  const cleanedUpRoles = roles.map((role) => {
+    delete role.users;
+    return role;
+  });
 
-  const adminRole = cleanedUpRoles.find(role => role.name === 'Administrator')
+  const adminRole = cleanedUpRoles.find(
+    (role) => role.name === "Administrator"
+  );
 
   // Admin role isn't touched.
-  const customRoles = cleanedUpRoles.filter(role => role.name !== 'Administrator')
+  const customRoles = cleanedUpRoles.filter(
+    (role) => role.name !== "Administrator"
+  );
+
   try {
-    const {data} = await api.post('roles', customRoles)
+    const { data }: { data: any } = await api.post("roles", customRoles);
   } catch {
     // maybe the roles already exist
   }
 
-  const adminUpdate = await api.patch(`roles/${adminRole.id}`, adminRole)
-  console.log('Seeded Roles')
+  const adminUpdate = await api.patch(`roles/${adminRole.id}`, adminRole);
+  console.log("Seeded Roles");
 }
