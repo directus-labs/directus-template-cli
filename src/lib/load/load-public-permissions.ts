2e64a88e9869c6b4ef882581218669175b7f9ccb
bryantgillespie <hey@bryantgillespie.com>
2023-10-16 09:45:04 -0400

    apply from dir WIP

--- src/lib/load/load-public-permissions.ts
+++ src/lib/load/load-public-permissions.ts
@@ -1,16 +1,23 @@
-import {api} from '../api'
-import loadToDestination from '../utils/load-to-destination'
+import { api } from "../api";
+import loadToDestination from "../utils/load-to-destination";
 
 async function removeallPublicPermissions() {
-  const {data}: {data} = await api.get('permissions?filter[role][_null]=true&limit=-1')
-  const ids = data.data.map(i => i.id)
-  if (!ids) return
-  await api.delete('permissions', {
+  const { data }: { data } = await api.get(
+    "permissions?filter[role][_null]=true&limit=-1"
+  );
+  console.log("Removing all public permissions", data.data);
+  const ids = data.data.map((i) => i.id);
+  if (!ids) return;
+  await api.delete("permissions", {
     data: ids,
-  })
+  });
 }
 
-export default async function loadPublicPermissions(roles: any) {
-  await removeallPublicPermissions()
-  await loadToDestination('permissions', roles)
+export async function loadPublicPermissions(roles: any) {
+  await removeallPublicPermissions();
+  await loadToDestination("permissions", roles);
+}
+
+export async function loadPermissions(roles: any) {
+  await loadToDestination("permissions", roles);
 }
