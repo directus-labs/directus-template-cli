a051b2fcd5d4634edc150a0aaffee4456de7d6c1
bryantgillespie <hey@bryantgillespie.com>
2024-01-29 20:03:33 -0500

    cleanup

--- src/lib/load/load-schema.ts
+++ src/lib/load/load-schema.ts
@@ -1,22 +1,27 @@
+import {schemaApply, schemaDiff} from '@directus/sdk'
+import {ux} from '@oclif/core'
 
-import {api} from '../api'
-import { ux } from '@oclif/core'
+import {api} from '../sdk'
+import logError from '../utils/log-error'
 import readFile from '../utils/read-file'
 
 export default async (dir: string) => {
+  ux.log('Loading schema • collections, fields, and relations...')
   const schema = readFile('snapshot', dir)
 
-  const {data}:{data} = await api.post('schema/diff?force', schema)
-  if (!data.data) {
-    console.log('No schema changes to apply')
+  // @ts-ignore
+  const data = await api.client.request(schemaDiff(schema, true))
+
+  if (!data) {
+    ux.log('No schema to apply')
     return
   }
 
   try {
-    await api.post('schema/apply', data.data)
+    await api.client.request(schemaApply(data))
+
     ux.log('Loaded schema')
-    // console.log('Schema Loaded')
   } catch (error) {
-    console.log('Error Applying schema', error.response.data.errors)
+    logError(error)
   }
 }
