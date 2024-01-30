b4fcf680f5f176e9ea01cbc3c5cdb36cff7e9837
bryantgillespie <hey@bryantgillespie.com>
2024-01-29 19:52:54 -0500

    utils

--- src/lib/utils/auth.ts
+++ src/lib/utils/auth.ts
@@ -1,30 +1,35 @@
-import { ux } from "@oclif/core";
-import { api } from "../api";
-import validateUrl from "./validate-url";
+import {readMe} from '@directus/sdk'
+import {ux} from '@oclif/core'
+
+import {api} from '../sdk'
+import validateUrl from './validate-url'
 
 export async function getDirectusUrl() {
-  const directusUrl = await ux.prompt("What is your Directus URL?");
+  const directusUrl = await ux.prompt('What is your Directus URL?', {default: 'http://localhost:8055'})
+
   // Validate URL
   if (!validateUrl(directusUrl)) {
-    ux.warn("Invalid URL");
-    return getDirectusUrl();
+    ux.warn('Invalid URL')
+    return getDirectusUrl()
   }
 
-  return directusUrl;
+  api.initialize(directusUrl)
+
+  return directusUrl
 }
 
 export async function getDirectusToken(directusUrl: string) {
-  const directusToken = await ux.prompt("What is your Directus Admin Token?");
+  const directusToken = await ux.prompt('What is your Directus Admin Token?')
+
   // Validate token
   try {
-    await api.get("/users/me", {
-      headers: {
-        Authorization: `Bearer ${directusToken}`,
-      },
-    });
-    return directusToken;
-  } catch {
-    ux.warn("Invalid token");
-    return getDirectusToken(directusUrl);
+    api.setAuthToken(directusToken)
+    const response = await api.client.request(readMe())
+    ux.log(`Logged in as ${response.first_name} ${response.last_name}`)
+    return directusToken
+  } catch (error) {
+    console.log(error)
+    ux.warn('Invalid token. Please try again.')
+    return getDirectusToken(directusUrl)
   }
 }
