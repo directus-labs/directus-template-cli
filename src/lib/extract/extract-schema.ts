5030a837a687cf8f6a0d6c17207b1a0ef5ecbb68
bryantgillespie <hey@bryantgillespie.com>
2024-02-08 15:25:43 -0500

    Refactor code and fix template validation issue

--- src/lib/extract/extract-schema.ts
+++ src/lib/extract/extract-schema.ts
@@ -9,15 +9,15 @@ import writeToFile from '../utils/write-to-file'
 export default async function extractSchema(dir: string) {
   const schemaDir = path.join(dir, 'schema')
 
-  // Check if directory for schema exists, if not, then create it.
-  if (!fs.existsSync(schemaDir)) {
-    console.log(`Attempting to create directory at: ${schemaDir}`)
-    fs.mkdirSync(schemaDir, {recursive: true})
-  }
-
   // Get the schema
   try {
-    const schema = api.client.request(schemaSnapshot())
+    // Check if directory for schema exists, if not, then create it.
+    if (!fs.existsSync(schemaDir)) {
+      console.log(`Attempting to create directory at: ${schemaDir}`)
+      fs.mkdirSync(schemaDir, {recursive: true})
+    }
+
+    const schema = await api.client.request(schemaSnapshot())
 
     // Write the schema to the specified directory
     await writeToFile('schema/snapshot', schema, dir)
--- src/lib/load/load-schema.ts
+++ src/lib/load/load-schema.ts
@@ -1,27 +1,111 @@
+/* eslint-disable unicorn/no-array-reduce  */
+/* eslint-disable perfectionist/sort-objects */
 import {schemaApply, schemaDiff} from '@directus/sdk'
 import {ux} from '@oclif/core'
+import * as inquirer from 'inquirer'
+import path from 'node:path'
 
 import {api} from '../sdk'
 import logError from '../utils/log-error'
 import readFile from '../utils/read-file'
 
-export default async (dir: string) => {
-  ux.log('Loading schema • collections, fields, and relations...')
-  const schema = readFile('snapshot', dir)
+export default async function loadSchema(dir: string) {
+  let diff
 
-  // @ts-ignore
-  const data = await api.client.request(schemaDiff(schema, true))
+  try {
+    const schemaDir = path.resolve(
+      dir,
+      'schema',
+    )
 
-  if (!data) {
-    ux.log('No schema to apply')
-    return
+    const schema = readFile('snapshot', schemaDir)
+
+    // @ts-ignore
+    diff = await api.client.request(schemaDiff(schema, true))
+
+    if (!diff) {
+      ux.log('No schema to apply')
+      return
+    }
+
+    const diffKeys = ['collections', 'fields', 'relations']
+
+    const changes = diffKeys.map(table => {
+      const tableDiff = diff.diff[table]
+
+      if (!tableDiff) {
+        return
+      }
+
+      const tableChanges = tableDiff.reduce((acc, item) => {
+        const {kind} = item.diff[0]
+        acc[DiffKind[kind]] = acc[DiffKind[kind]] ? acc[DiffKind[kind]] + 1 : 1
+        return acc
+      }, {})
+
+      return {
+        table,
+        ...tableChanges,
+      }
+    })
+
+    // Display the changes to be applied in a table using the ux.table method. The table should have the following columns: Table, New, Edit, Delete
+    ux.log('Schema diff summary from current instance:')
+    ux.table(changes, {
+      table: {
+        get: (row: any) => row.table,
+        minWidth: 20,
+      },
+      create: {
+        get: (row: any) => row.new || 0,
+        minWidth: 5,
+
+      },
+      update: {
+        get: (row: any) => row.edit || 0,
+        minWidth: 5,
+      },
+      delete: {
+        get: (row: any) => row.delete || 0,
+        minWidth: 5,
+      },
+
+    })
+  } catch (error) {
+    logError(error)
+  }
+
+  const proceed = await inquirer.prompt([{
+    message: 'Applying . This cannot be undone. Continue?',
+    name: 'continue',
+    type: 'confirm',
+
+  }])
+
+  if (!proceed.continue) {
+    ux.log('Cancelled applying schema changes and exiting...')
+    ux.exit(0)
   }
 
   try {
-    await api.client.request(schemaApply(data))
+    await api.client.request(schemaApply(diff))
 
     ux.log('Loaded schema')
   } catch (error) {
     logError(error)
   }
+}
+
+/**
+ * Indicates the kind of change based on comparisons by deep-diff package
+ */
+export const DiffKind = {
+  /** indicates a change occurred within an array */
+  A: 'array',
+  /** indicates a property/element was deleted */
+  D: 'delete',
+  /** indicates a property/element was edited */
+  E: 'edit',
+  /** indicates a newly added property/element */
+  N: 'new',
 }
