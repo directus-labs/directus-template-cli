aeb0a8d18a89679b2f6a544cadf0ed751a76fa55
bryantgillespie <hey@bryantgillespie.com>
2024-09-10 03:22:20 -0400

    use unjs defu for project settings

--- src/lib/load/load-settings.ts
+++ src/lib/load/load-settings.ts
@@ -1,4 +1,6 @@
-import {updateSettings} from '@directus/sdk'
+import {readSettings,updateSettings} from '@directus/sdk'
+import { defu } from "defu";
+
 
 import {api} from '../sdk'
 import logError from '../utils/log-error'
@@ -6,9 +8,14 @@ import readFile from '../utils/read-file'
 
 export default async function loadSettings(dir: string) {
   const settings = readFile('settings', dir)
+
+
   try {
+    // Get the current settings and merge them with current settings as defaults. To prevent overriding any logos, themes, etc.
+    const currentSettings = await api.client.request(readSettings())
+    const mergedSettings = defu(currentSettings, settings)
     // @ts-ignore
-    await api.client.request(updateSettings(settings))
+    await api.client.request(updateSettings(mergedSettings))
   } catch (error) {
     logError(error)
   }
