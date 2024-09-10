15503ffe9147b04bce77dc836def7cd8cfb65ca0
bryantgillespie <hey@bryantgillespie.com>
2024-09-10 16:34:36 -0400

    settings types

--- src/lib/load/load-settings.ts
+++ src/lib/load/load-settings.ts
@@ -1,6 +1,8 @@
-import {readSettings,updateSettings} from '@directus/sdk'
-import { defu } from "defu";
 
+import type {DirectusSettings} from '@directus/sdk'
+
+import {readSettings, updateSettings} from '@directus/sdk'
+import {defu} from 'defu'
 
 import {api} from '../sdk'
 import logError from '../utils/log-error'
@@ -9,12 +11,10 @@ import readFile from '../utils/read-file'
 export default async function loadSettings(dir: string) {
   const settings = readFile('settings', dir)
 
-
   try {
     // Get the current settings and merge them with current settings as defaults. To prevent overriding any logos, themes, etc.
     const currentSettings = await api.client.request(readSettings())
-    const mergedSettings = defu(currentSettings, settings)
-    // @ts-ignore
+    const mergedSettings = defu(currentSettings, settings) as DirectusSettings
     await api.client.request(updateSettings(mergedSettings))
   } catch (error) {
     logError(error)
